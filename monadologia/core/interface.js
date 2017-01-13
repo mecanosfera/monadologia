ad43a769505a5a53e23ec289d81a48decc72d866
Carlos <carloscardosof@gmail.com>
2017-01-13 00:06:12 -0200

    Small fixes

--- monadologia/core/interface.js
+++ monadologia/core/interface.js
@@ -77,8 +77,9 @@ class UI{
 	carregar(){
 		$('#script_estado_inicial').remove();
 		$('#script_celula').remove();
-		$('body').append('<script type="text/javascript" id="script_estado_inicial" src="'+$('#estado_inicial').val()+'"><\/script>');
 		$('body').append('<script type="text/javascript" id="script_celula" src="'+$('#celula').val()+'"><\/script>');
+		$('body').append('<script type="text/javascript" id="script_estado_inicial" src="'+$('#estado_inicial').val()+'"><\/script>');
+		
 		
 	}
 	
